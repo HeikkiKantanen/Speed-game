bd6ccb8335a35069bd4e982324ff1e1f2f819e79
HeikkiKantanen <hessu.kantanen@gmail.com>
2021-11-15 13:59:46 +0200

    updated version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,76 @@
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Circle from "./Circle";
+import { circles } from "./circles";
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+const getRndInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+
+class App extends Component {
+  state = {
+    score: 0,
+    current: 0,
+  };
+
+  timer = undefined;
+  pace = 1500;
+
+  
+  clickHandler = () => {
+    this.setState ({
+      score: this.state.score + 10,
+    });
+  };
+
+  nextCircle = () => {
+    let nextActive;
+
+    do {
+      nextActive = getRndInteger(1, 4)
+    } while (nextActive === this.state.current);
+
+    this.setState({
+      current: nextActive,
+    });
+    this.pace *= 0.95;
+    this.timer = setTimeout (this.nextCircle, this.pace);
+
+    console.log("active circle is ", this.state.current)
+  };
+
+  startHandler = () => {
+    this.nextCircle();
+  };
+
+  stopHandler = () => {
+    clearTimeout(this.timer);
+  };
+  
+    render() {
+      return (
+        <div>
+          <h1>Speed Game</h1>
+            <div className="circle-wrapper">
+              {circles.map((c) => (
+              <Circle 
+                key={c.id} 
+                color={c.color} 
+                id={c.id}
+                click={this.clickHandler} 
+                active={this.state.current === c.id}
+              />
+              ))}
+            </div>
+              <p>Your Score:</p>
+            <div>
+              <button onClick={this.startHandler}>Start</button>
+              <button onClick={this.stopHandler}>Stop</button>
+            </div>
+        </div>
+      );
+    };
+  };
 
 export default App;
---
+++ src/circles.js
@@ -0,0 +1,6 @@
+export const circles =  [
+    {id:1, color: "aqua"},
+    {id:2, color: "dodger blue"},
+    {id:3, color: "coral"},
+    {id:4, color: "yellowgreen"},
+  ];
