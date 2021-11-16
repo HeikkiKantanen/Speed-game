0bcfa5a5a124ba13b43ee633a7fb590f53dc1868
HeikkiKantanen <hessu.kantanen@gmail.com>
2021-11-16 14:50:50 +0200

    updated version with sounds and images

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import Circle from "./Circle";
 import { circles } from "./circles";
 import GameOver from "./GameOver";
 
+import startSound from "./assets/sounds/one.wav"
+import endSound from "./assets/sounds/Death-of-a-Ninja-(Game-Over).mp3"
+import clickSound from "./assets/sounds/boxer.wav"
+
+let gameStartSound = new Audio(startSound);
+let gameEndSound = new Audio(endSound);
+let gameClickSound = new Audio(clickSound);
+
+
 const getRndInteger = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
@@ -13,19 +22,45 @@ class App extends Component {
     score: 0,
     current: 0,
     gameOver: false,
+    pace: 1500,
+    rounds: 0,
+    gameStart: false
   };
 
   timer = undefined;
-  pace = 1500;
 
+  clickPlay = () => {
+      if(gameClickSound.paused) {
+        gameClickSound.play();
+      } else {
+        gameClickSound.currentTime = 0;
+      }
+    };
   
-  clickHandler = () => {
+
+  clickHandler = (id) => {
+    this.clickPlay();
+
+    console.log("you clicked: ", id);
+
+    if(this.state.current !== id) {
+      this.stopHandler();
+      return;
+    }
+
     this.setState ({
       score: this.state.score + 10,
+      rounds: 0,
     });
   };
 
   nextCircle = () => {
+
+    if(this.state.rounds >= 5) {
+      this.stopHandler();
+      return;
+    }
+
     let nextActive;
 
     do {
@@ -34,44 +69,63 @@ class App extends Component {
 
     this.setState({
       current: nextActive,
+      pace: this.state.pace * 0.95,
+      rounds: this.state.rounds + 1,
     });
-    this.pace *= 0.95;
-    this.timer = setTimeout (this.nextCircle, this.pace);
+
+    this.timer = setTimeout (this.nextCircle, this.state.pace);
   };
 
   startHandler = () => {
+    gameStartSound.play();
     this.nextCircle();
+    this.setState({
+      gameStart: true,
+    });
   };
 
   stopHandler = () => {
+    gameStartSound.pause();
+    gameEndSound.play();
     clearTimeout(this.timer);
 
     this.setState({
       gameOver: true,
+      current: 0,
+      gameStart: false,
     });
   };
 
-
+  closeHandler = () => {
+    gameEndSound.pause();
+    this.setState({
+      gameOver: false,
+      score: 0,
+      pace: 1500,
+      rounds: 0,
+    });
+  };
 
     render() {
       return (
         <div>
-          {this.state.gameOver && <GameOver score={this.state.score} />}
-          <h1>Speed Game</h1>
+          {this.state.gameOver && <GameOver score={this.state.score} close={this.closeHandler} />}
+          <h1>Punch the Burglar</h1>
             <div className="circle-wrapper">
               {circles.map((c) => (
               <Circle 
                 key={c.id} 
                 color={c.color} 
                 id={c.id}
-                click={this.clickHandler} 
+                click={() => this.clickHandler(c.id)} 
                 active={this.state.current === c.id}
+                disabled={this.state.gameStart}
               />
               ))}
             </div>
               <p className="score">Your Score: {this.state.score}</p>
             <div>
-              <button onClick={this.startHandler}>Start</button>
+              <button disabled={this.state.gameStart} onClick={this.startHandler}>Start</button>
               <button onClick={this.stopHandler}>Stop</button>
             </div>
         </div>
--- src/GameOver.js
+++ src/GameOver.js
@@ -1,14 +1,11 @@
 import React from "react";
 
-const closeHandler = () => {
-    window.location.reload();
-};
 
 const GameOver = (props) => {
     return (
         <div className="popup-wrapper">
             <div className="popup">
-                <button onClick={closeHandler}>X</button>
+                <button onClick={props.close}>X</button>
                 <h2>Game over</h2>
                 <p> Your Score: {props.score} </p>
             </div>
